ebbe1b2e715dcd6f59611c689840c943f63790ba
Shr4528 <Shr4528@gmail.com>
2022-12-16 19:10:25 +0200

    commit

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+import * as state from '../store/state'
+import * as actions from '../store/actions'
+import * as mutations from '../store/mutations'
+import * as getters from '../store/getters'
+
+
+const store = createStore({
+    state,
+    actions,
+    mutations,
+    getters,
+})
+
+export default store;
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="p-8 pb-0">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search Meals"
+    />
+    <div class="lex flex-wrap justify-center gap-3 px-8 mb-6">
+      <routet-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter of letters"
+        :key="letter"
+      >
+        {{ letter }}
+      </routet-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, onMounted } from 'vue';
+import store from '../store';
+import axiosClient from '../axiosClient.js';
+
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(' ');
+
+onMounted(async () => {
+  const response = await axiosClient.get('/list.php?i=list');
+  console.log(response);
+});
+</script>
